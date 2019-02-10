9477be5e9f737400744b62145370c94119c5df4b
ijs9200 <ijs9200@naver.com>
2019-02-10 20:06:04 +0900

    first

---
+++ src/com/biz/grade/vo/GradeVO.java
@@ -0,0 +1,55 @@
+package com.biz.grade.vo;
+
+public class GradeVO {
+private String strNum;
+private String strName;
+private String strTel;
+private String strAddr;
+
+int intKor;
+int intEng;
+int intMath;
+public String getStrNum() {
+	return strNum;
+}
+public void setStrNum(String strNum) {
+	this.strNum = strNum;
+}
+public String getStrName() {
+	return strName;
+}
+public void setStrName(String strName) {
+	this.strName = strName;
+}
+public String getStrTel() {
+	return strTel;
+}
+public void setStrTel(String strTel) {
+	this.strTel = strTel;
+}
+public String getStrAddr() {
+	return strAddr;
+}
+public void setStrAddr(String strAddr) {
+	this.strAddr = strAddr;
+}
+public int getIntKor() {
+	return intKor;
+}
+public void setIntKor(int intKor) {
+	this.intKor = intKor;
+}
+public int getIntEng() {
+	return intEng;
+}
+public void setIntEng(int intEng) {
+	this.intEng = intEng;
+}
+public int getIntMath() {
+	return intMath;
+}
+public void setIntMath(int intMath) {
+	this.intMath = intMath;
+}
+
+}
